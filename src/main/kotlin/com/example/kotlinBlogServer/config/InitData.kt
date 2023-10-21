ece5b783aa79ad92e28ca7e6ef9c63d0fd2c8d4d
meureuet <gmdgjfr@gmail.com>
2023-10-21 16:59:10 +0900

    더미데이터 멤버/포스트 100개씩

--- src/main/kotlin/com/example/kotlinBlogServer/config/InitData.kt
+++ src/main/kotlin/com/example/kotlinBlogServer/config/InitData.kt
@@ -1,6 +1,10 @@
 package com.example.kotlinBlogServer.config
 
 import com.example.kotlinBlogServer.domain.member.*
+import com.example.kotlinBlogServer.domain.post.Post
+import com.example.kotlinBlogServer.domain.post.PostRepository
+import com.example.kotlinBlogServer.domain.post.PostSaveReq
+import com.example.kotlinBlogServer.domain.post.toEntity
 import io.github.serpro69.kfaker.faker
 import mu.two.KotlinLogging
 import org.springframework.boot.context.event.ApplicationReadyEvent
@@ -11,7 +15,8 @@ import org.springframework.context.event.EventListener
 
 @Configuration
 class InitData(
-    private val memberRepository: MemberRepository
+    private val memberRepository: MemberRepository,
+    private val postRepository: PostRepository
 ) {
     val faker = faker { }
     private val log = KotlinLogging.logger { }
@@ -21,23 +26,50 @@ class InitData(
     private fun init(){
         log.info("와 로그다")
 
+        val members = generateMembers(100)
+        memberRepository.saveAll(members)
+
+        val posts = generatePosts(100)
+        postRepository.saveAll(posts)
+    }
+
+    private fun generateMembers(cnt: Int) : MutableList<Member> {
         // arraylist 같은 친구
         val members = mutableListOf<Member>()
 
-        for (i in 1..100){
+        for (i in 1..cnt){
             val member = generateMember()
-            log.info("시작 멤버 로그 $member")
+            log.info("더미 멤버 $member")
             members.add(member)
         }
 
-        memberRepository.saveAll(members)
+        return members
+    }
+
+    private fun generatePosts(cnt: Int) : MutableList<Post> {
+        // arraylist 같은 친구
+        val posts = mutableListOf<Post>()
+
+        for (i in 1..cnt){
+            val post = generatePost()
+            log.info("더미 포스트 $post")
+            posts.add(post)
+        }
+
+        return posts
     }
 
     // 코틀린은 마지막 변수가 리턴임
     // 바로 리턴 ㄱ
-    private fun generateMember(): Member = MemberSaveDto(
-            email = faker.internet.safeEmail(),
-            password = "1234",
-            role = Role.USER
-        ).toEntity()
+    private fun generateMember(): Member = MemberSaveReq(
+        email = faker.internet.safeEmail(),
+        password = "1234",
+        role = Role.USER
+    ).toEntity()
+
+    private fun generatePost(): Post = PostSaveReq(
+        title = faker.theExpanse.ships(),
+        content = faker.quote.matz(),
+        memberId = 1
+    ).toEntity()
 }
