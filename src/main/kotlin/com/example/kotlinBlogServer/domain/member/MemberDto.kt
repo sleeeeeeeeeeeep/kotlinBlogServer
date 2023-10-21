6e9729b0b11c8602b4d9fdbb0c5cc69c78261067
meureuet <gmdgjfr@gmail.com>
2023-10-21 15:52:25 +0900

    멤버 저장용 dto

---
+++ src/main/kotlin/com/example/kotlinBlogServer/domain/member/MemberDto.kt
@@ -0,0 +1,20 @@
+package com.example.kotlinBlogServer.domain.member
+
+/**
+ * dto <-> entity 간 매핑 스타일
+ * 1. 각 dto, entity 마다 매핑로직 설정
+ * 2. 인터페이스 하나 만들어서 사용
+ */
+data class MemberSaveDto(
+    val email: String,
+    val password: String,
+    val role: Role,
+)
+
+fun MemberSaveDto.toEntity(): Member {
+    return Member(
+        email = this.email,
+        password = this.password,
+        role = this.role
+    )
+}
