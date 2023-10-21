2ff06b80bf131a37892604cd5b8e86053d28e41f
meureuet <gmdgjfr@gmail.com>
2023-10-21 16:58:50 +0900

    서비스

--- src/main/kotlin/com/example/kotlinBlogServer/service/MemberService.kt
+++ src/main/kotlin/com/example/kotlinBlogServer/service/MemberService.kt
@@ -2,6 +2,8 @@ package com.example.kotlinBlogServer.service
 
 import com.example.kotlinBlogServer.domain.member.Member
 import com.example.kotlinBlogServer.domain.member.MemberRepository
+import com.example.kotlinBlogServer.domain.member.MemberRes
+import com.example.kotlinBlogServer.domain.member.toDto
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -11,6 +13,9 @@ class MemberService(
 ) {
 
     @Transactional(readOnly = true)
-    fun findAll(): MutableList<Member> = memberRepository.findAll()
+    fun findAll(): List<MemberRes> =
+        memberRepository.findAll().map {
+            it.toDto()
+        }
 
 }
