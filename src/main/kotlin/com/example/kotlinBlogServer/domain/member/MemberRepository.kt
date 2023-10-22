57e31bf7801956eba212889228ddc064feded734
meureuet <gmdgjfr@gmail.com>
2023-10-22 11:21:23 +0900

    querydsl 인터페이스만 추가

--- src/main/kotlin/com/example/kotlinBlogServer/domain/member/MemberRepository.kt
+++ src/main/kotlin/com/example/kotlinBlogServer/domain/member/MemberRepository.kt
@@ -1,7 +1,19 @@
 package com.example.kotlinBlogServer.domain.member
 
+import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 interface MemberRepository : JpaRepository<Member, Long>{
+}
+
+@Repository
+interface MemberCustomRepository {
+}
+
+class MemeberCustomRepositoryImpl(
+
+    private val jpaQueryFactory: JPAQueryFactory
+
+): MemberCustomRepository {
 }
