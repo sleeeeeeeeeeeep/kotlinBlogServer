8e465212b8434b45bb980450d2c5c12c4a3d8d91
meureuet <gmdgjfr@gmail.com>
2023-11-27 23:27:56 +0900

    날짜 포맷

--- src/main/kotlin/com/example/kotlinBlogServer/config/ObjectMapperConfig.kt
+++ src/main/kotlin/com/example/kotlinBlogServer/config/ObjectMapperConfig.kt
@@ -1,20 +1,31 @@
 package com.example.kotlinBlogServer.config
 
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.*
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinFeature
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.addDeserializer
+import com.fasterxml.jackson.module.kotlin.addSerializer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 @Configuration
 class ObjectMapperConfig {
     @Bean
-    fun objectMapper() : ObjectMapper{
+    fun objectMapper(): ObjectMapper {
         val mapper = ObjectMapper()
-        mapper.registerModules(JavaTimeModule())
+        val javaTimeModule = JavaTimeModule()
+
+        javaTimeModule.addSerializer(LocalDateTime::class, CustomLocalDateTimeSerializer())
+
+        mapper.registerModules(javaTimeModule)
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
 
         mapper.registerModules(
             KotlinModule.Builder()
@@ -28,4 +39,13 @@ class ObjectMapperConfig {
 
         return mapper
     }
+
+    class CustomLocalDateTimeSerializer : JsonSerializer<LocalDateTime>() {
+        private val dateTimeFormat = "yyyy-MM-dd HH:mm:ss"
+        private val formatter = DateTimeFormatter.ofPattern(dateTimeFormat, Locale.KOREA)
+        override fun serialize(value: LocalDateTime, gen: JsonGenerator, serializers: SerializerProvider) {
+            gen.writeString(formatter.format(value))
+        }
+    }
+
 }
